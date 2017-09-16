10380cb05722557b3d9b3517212c8543382dd35c
Petr Messner <petr.messner@gmail.com>
2017-09-16 09:03:20 +0200

    model

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Publication (models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+
+class Comment (models.Model):
+    author_name = models.CharField(max_length=100)
+    body = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '{}: {}'.format(self.author_name, body)
+
+
+class Article (models.Model):
+    headline = models.CharField(max_length=100)
+    publications = models.ManyToManyField(Publication)
+    comments = models.ManyToManyField(Comment)
+
+    def __str__(self):
+        return self.headline
+
+    class Meta:
+        ordering = ('headline',)
