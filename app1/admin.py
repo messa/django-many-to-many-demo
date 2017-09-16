d80e2250f66b196532b98f404df9fd2f98bf2c4e
Petr Messner <petr.messner@gmail.com>
2017-09-16 09:10:40 +0200

    admin

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Publication, Comment, Article
+
+
+class PublicationAdmin (admin.ModelAdmin):
+    pass
+
+admin.site.register(Publication, PublicationAdmin)
+
+
+class CommentAdmin (admin.ModelAdmin):
+    pass
+
+admin.site.register(Comment, CommentAdmin)
+
+
+class ArticleAdmin (admin.ModelAdmin):
+    pass
+
+admin.site.register(Article, ArticleAdmin)
