6d8acc70491a1b2938ec47ec4849bb376a6453ff
Petr Messner <petr.messner@gmail.com>
2017-09-16 08:59:36 +0200

    settings

--- proj1/settings.py
+++ proj1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vh_-7h(hl&1*6y)!-%=@pql%3m9u^)oavzz-=c0cg@4*xp)uhz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if os.environ.get('PRODUCTION') else True
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app1',
 ]
 
 MIDDLEWARE = [
